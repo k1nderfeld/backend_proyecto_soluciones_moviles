5b9e4fc7138d4212041d47b2b5dfc3dc3965da6d
zeosjb <jose.benitez@alumnos.ucn.cl>
2026-09-03 16:09:06 -0400

    feat: initialize backend project with Express and necessary dependencies
    - Added package.json with project metadata and dependencies
    - Created directory structure for config, controllers, database, middlewares, models, routes, services, and utils
    - Implemented basic server setup in src/server.js with Express, Morgan, and CORS
    - Established a placeholder for database connection and routes

---
+++ src/server.js
@@ -0,0 +1,57 @@
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+require("dotenv").config()
+
+// Imports
+
+class Server {
+    constructor() {
+        this.app =  express();
+        this.port = process.env.PORT || 8080;
+        this.server = require("http").createServer(this.app);
+
+        // Paths
+        this.paths = {
+            // A integración en la siguiente clase
+        }
+
+        // Conexión con la base de datos
+        this.connectDB();
+
+        // JSON
+        this.app.use(express.json());
+
+        // Middlewares
+        this.middlewares();
+
+        // Routes
+        this.routes();
+    }
+
+    async connectDB() {
+        await db.authenticate().then(() => {
+            console.log("Base de datos se ha conectado satisfactoriamente.")
+        }).catch((err) => {
+            console.error("No se ha podido establecer la conexión con la base de datos.", err)
+        })
+
+        // Carga de modelos a la base de datos
+    }
+
+    middlewares() {
+
+    }
+
+    routes() {
+
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`El servidor esta corriendo en la url: http://localhost:${this.port}`)
+        })
+    }
+}
+
+module.exports = Server;
